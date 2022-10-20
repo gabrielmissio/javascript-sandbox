f2b68e2a2d00a2f26537b00e9ea2caf463265f31
gabrielmissio <gabrielmissio.dev@gmail.com>
2022-10-20 13:30:56 -0300

    feat: add countDown to whac-a-mole demo

--- whac-a-mole/app.js
+++ whac-a-mole/app.js
@@ -1,16 +1,14 @@
 const scoreDisplay = document.querySelector('#score')
+const timeLeftDisplay = document.querySelector('#time-left')
 const squares = document.querySelectorAll('.square')
+let timeLeft = 15
 let score = 0
 let molePosition
+let moleId
+let timeLeftId
 
-for (const square of squares) {
-  square.addEventListener('mousedown', (e) => {
-    if (e.target.id === molePosition) {
-      score += 1
-      scoreDisplay.innerHTML = score
-    }
-  })
-}
+scoreDisplay.innerHTML = score
+timeLeftDisplay.innerHTML = timeLeft
 
 function moveMoleToRandomSquare() {
   for (const square of squares) {
@@ -24,4 +22,36 @@ function moveMoleToRandomSquare() {
   molePosition = square.id
 }
 
-setInterval(moveMoleToRandomSquare, 500)
+function moleClickHandler(e) {
+  if (e.target.id === molePosition) {
+    score += 1
+    scoreDisplay.innerHTML = score
+  }
+}
+
+function countDown() {
+  timeLeft -= 1
+  timeLeftDisplay.innerHTML = timeLeft
+
+  if (timeLeft === 0) stop()
+}
+
+function start() {
+  for (const square of squares) {
+    square.addEventListener('mousedown', moleClickHandler)
+  }
+
+  moleId = setInterval(moveMoleToRandomSquare, 500)
+  timeLeftId = setInterval(countDown, 1000)
+}
+
+function stop() {
+  for (const square of squares) {
+    square.removeEventListener('mousedown', moleClickHandler)
+  }
+
+  clearInterval(timeLeftId)
+  clearInterval(moleId)
+}
+
+start()
