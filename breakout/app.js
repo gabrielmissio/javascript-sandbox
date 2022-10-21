a76bd8c9287ed2ba349aec49b60da1d49929dd16
gabrielmissio <gabrielmissio.dev@gmail.com>
2022-10-21 12:21:21 -0300

    feat: add moveBall to breakout demo

--- breakout/app.js
+++ breakout/app.js
@@ -2,11 +2,17 @@ const gridDisplay = document.querySelector('.grid')
 const userDisplay = document.createElement('div')
 const ballDisplay = document.createElement('div')
 const gridWidth = 560
+const gridHeight = 300
 const blockWidth = 70
 const blockHeight = 10
 const userInitialPosition = [240, 50]
 const ballInitialPosition = [265, 220]
+const ballDiameter = 20
+let userCurrentPosition = null
+let ballId = null
 let ballCurrentPosition = null
+let ballCurrentDirectionX = null
+let ballCurrentDirectionY = null
 
 class Block {
     constructor(xAxis, yAxis) {
@@ -32,11 +38,11 @@ const blocks = [
 
 function createBlocks() {
     for (const block of blocks) {
-        const displayBlock = document.createElement('div')
-        displayBlock.classList.add('block')
-        displayBlock.style.left = `${block.bottomLeft[0]}px`
-        displayBlock.style.bottom = `${block.bottomLeft[1]}px`
-        gridDisplay.appendChild(displayBlock)
+        const blockDisplay = document.createElement('div')
+        blockDisplay.classList.add('block')
+        blockDisplay.style.left = `${block.bottomLeft[0]}px`
+        blockDisplay.style.bottom = `${block.bottomLeft[1]}px`
+        gridDisplay.appendChild(blockDisplay)
     }
 }
 
@@ -57,9 +63,16 @@ function createUser() {
     gridDisplay.appendChild(userDisplay)
 }
 
+function randomNumber(min, max) { 
+    const result = Math.random() * (max - min) + min
+    return result
+}
+
 function createBall() {
     ballDisplay.classList.add('ball')
     ballCurrentPosition = ballInitialPosition
+    ballCurrentDirectionY = 1
+    ballCurrentDirectionX = randomNumber(0, 1) %2 === 0 ? 1 : -1
     drawBall()
     gridDisplay.appendChild(ballDisplay)
 }
@@ -81,12 +94,65 @@ function moveUser(e) {
     }
 }
 
-function moveBall() {
-    if(ballCurrentPosition[1] > 0) {
-        ballCurrentPosition[0] -= 1
-        ballCurrentPosition[1] -= 2
-        drawBall()
+function changeBallDirection() {
+    // ball falling
+    if (ballCurrentDirectionY === -1 && ballCurrentDirectionX === 1) {
+        ballCurrentDirectionX = -1
+        return
     }
+    if (ballCurrentDirectionY === -1 && ballCurrentDirectionX === -1) {
+        ballCurrentDirectionY = 1
+        return
+    }
+
+    // ball rising
+    if (ballCurrentDirectionY === 1 && ballCurrentDirectionX === 1) {
+        ballCurrentDirectionY = -1
+        return
+    }
+    if (ballCurrentDirectionY === 1 && ballCurrentDirectionX === -1) {
+        ballCurrentDirectionX = 1
+        return
+    }
+}
+
+function hitBall() {
+    ballCurrentDirectionY = 1
+}
+
+function checkBallCollision() {
+    // check grid collision
+    if (
+        ballCurrentPosition[1] >= gridHeight - ballDiameter ||
+        ballCurrentPosition[0] <= 0 ||
+        ballCurrentPosition[0] >= gridWidth - ballDiameter
+    ) {
+        changeBallDirection()
+    }
+
+    // check user collision
+    if (
+        ballCurrentPosition[1] === userCurrentPosition[1] + 10 &&
+        (
+            ballCurrentPosition[0] <= userCurrentPosition[0] + blockWidth &&
+            ballCurrentPosition[0] >= userCurrentPosition[0]
+        )
+    ) {
+        hitBall()
+    }
+
+    // check floor collision
+    if (ballCurrentPosition[1] <= 0) {
+        alert('game over')
+        clearInterval(ballId)
+    }
+}
+
+function moveBall() {
+    ballCurrentPosition[0] += ballCurrentDirectionX
+    ballCurrentPosition[1] += ballCurrentDirectionY
+    drawBall()
+    checkBallCollision()
 }
 
 function start() {
@@ -94,7 +160,7 @@ function start() {
     createBlocks()
     createUser()
     createBall()
-    setInterval(moveBall, 12)
+    ballId = setInterval(moveBall, 7)
 }
 
 start()
